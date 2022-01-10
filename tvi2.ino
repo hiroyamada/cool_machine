2df87c8ee2f28215007687d9a6079ed1d8b953ed
Hiro Yamada <hirodotyamada@gmail.com>
2022-01-10 16:33:22 -0500

    added voice functionality

--- tvi2.ino
+++ tvi2.ino
@@ -1,3 +1,13 @@
+#include <Arduino.h>
+#include "Talkie.h"
+#include "Vocab_US_Large.h"
+#include "Vocab_Special.h"
+#include "Vocab_US_Acorn.h"
+#include "Vocab_US_TI99.h"
+#include "Vocab_US_Clock.h"
+
+Talkie voice(true, false);
+
 const int NUM_MAPPINGS = 18;
 const int SENSOR_PINS[8] = {6, 7, 8, 9, 10, 11, 12, 13};
 
@@ -46,7 +56,18 @@ void setupSensors() {
 }
 
 void setupSpeakers() {
-  // TODO: setup speaker pins  
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(115200);
+#if defined(__AVR_ATmega32U4__) || defined(SERIAL_USB) || defined(SERIAL_PORT_USBVIRTUAL)  || defined(ARDUINO_attiny3217)
+    delay(4000); // To be able to connect Serial monitor after reset or power up and before first print out. Do not wait for an attached Serial Monitor!
+#endif
+    // Just to know which program is running on my Arduino
+    Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_TALKIE));
+    // voice.doNotUseInvertedOutput();
+#if defined(TEENSYDUINO)
+        pinMode(5, OUTPUT);
+        digitalWrite(5, HIGH); //Enable Amplified PROP shield
+    #endif
 }
 
 void loop() {
@@ -82,9 +103,65 @@ void updateLetter() {
 }
 
 void speakCharacter() {
-  // TODO: output intepreted_character to speakers  
-//  Serial.print("index_tip, middle_tip, ring_tip, pinky_tip, index_joint, middle_joint, ring_joint, pinky_joint");
-//  Serial.println("");
+      if (interpreted_letter == "a") {
+      voice.say(spa_A);
+    }
+    else if (interpreted_letter == "d") {
+      voice.say(spa_D);
+    }
+    else if (interpreted_letter == "f") {
+      voice.say(spa_F);
+    }
+    else if (interpreted_letter == "g") {
+      voice.say(spa_G);
+    }
+    else if (interpreted_letter == "h") {
+      voice.say(spa_H);
+    }
+    else if (interpreted_letter == "i") {
+      voice.say(spa_I);
+    }
+    else if (interpreted_letter == "k/p") {
+      voice.say(spa_K);
+      voice.say(spa_OR);
+      voice.say(spa_P);
+    }
+    else if (interpreted_letter == "l/q") {
+      voice.say(spa_L);
+      voice.say(spa_OR);
+      voice.say(spa_Q);
+    }
+    else if (interpreted_letter == "m") {
+      voice.say(spa_M);
+    }
+    else if (interpreted_letter == "n") {
+      voice.say(spa_N);
+    }
+    else if (interpreted_letter == "o") {
+      voice.say(spa_O);
+    }
+    else if (interpreted_letter == "r/u/v") {
+      voice.say(spa_R);
+      voice.say(spa_OR);
+      voice.say(spa_U);
+      voice.say(spa_OR);
+      voice.say(spa_V);
+    }
+    else if (interpreted_letter == "s") {
+      voice.say(spa_S);
+    }
+    else if (interpreted_letter == "t") {
+      voice.say(spa_T);
+    }
+    else if (interpreted_letter == "w") {
+      voice.say(spa_W);
+    }
+    else if (interpreted_letter == "x") {
+      voice.say(spa_X);
+    }
+    else if (interpreted_letter == "y") {
+      voice.say(spa_Y);
+    }
   Serial.print(interpreted_letter);
   Serial.print(": ");
   for (int i = 0; i<num_sensors; i++) {
