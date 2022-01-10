d07aa52f8b7be0e71095a547ee5c341ba6d247e9
Hiro Yamada <hirodotyamada@gmail.com>
2022-01-10 16:34:46 -0500

    format code

--- tvi2.ino
+++ tvi2.ino
@@ -23,23 +23,23 @@ struct letter_sensor_mapping {
 
 // {index_tip, middle_tip, ring_tip, pinky_tip, index_joint, middle_joint, ring_joint, pinky_joint}
 letter_sensor_mapping letter_sensor_mappings[NUM_MAPPINGS] = {
-{"a", {0,0,0,0,0,1,1,1}},
-{"d", {1,0,1,1,1,1,1,1}},
-{"f", {0,1,1,1,1,1,1,1}},
-{"g", {1,0,0,0,1,0,1,1}},
-{"h", {1,1,0,0,1,1,0,1}},
-{"i", {0,0,0,1,0,1,1,1}},
-{"k/p", {1,1,0,0,1,0,1,1}},
-{"l/q", {1,0,0,0,1,1,1,1}},
-{"m", {0,0,0,0,1,1,1,0}},
-{"n", {0,0,0,0,1,1,0,1}},
-{"o", {0,0,1,1,1,1,1,1}},
-{"r/u/v", {1,1,0,0,1,1,0,1}},
-{"s", {0,0,0,0,1,1,1,1}},
-{"t", {0,0,0,0,1,0,1,1}},
-{"w", {1,1,1,1,1,1,1,0}},
-{"x", {1,0,0,0,1,0,1,1}},
-{"y", {0,0,0,1,1,1,1,1}}
+  {"a", {0, 0, 0, 0, 0, 1, 1, 1}},
+  {"d", {1, 0, 1, 1, 1, 1, 1, 1}},
+  {"f", {0, 1, 1, 1, 1, 1, 1, 1}},
+  {"g", {1, 0, 0, 0, 1, 0, 1, 1}},
+  {"h", {1, 1, 0, 0, 1, 1, 0, 1}},
+  {"i", {0, 0, 0, 1, 0, 1, 1, 1}},
+  {"k/p", {1, 1, 0, 0, 1, 0, 1, 1}},
+  {"l/q", {1, 0, 0, 0, 1, 1, 1, 1}},
+  {"m", {0, 0, 0, 0, 1, 1, 1, 0}},
+  {"n", {0, 0, 0, 0, 1, 1, 0, 1}},
+  {"o", {0, 0, 1, 1, 1, 1, 1, 1}},
+  {"r/u/v", {1, 1, 0, 0, 1, 1, 0, 1}},
+  {"s", {0, 0, 0, 0, 1, 1, 1, 1}},
+  {"t", {0, 0, 0, 0, 1, 0, 1, 1}},
+  {"w", {1, 1, 1, 1, 1, 1, 1, 0}},
+  {"x", {1, 0, 0, 0, 1, 0, 1, 1}},
+  {"y", {0, 0, 0, 1, 1, 1, 1, 1}}
 };
 
 void setup() {
@@ -49,45 +49,45 @@ void setup() {
 }
 
 void setupSensors() {
-  for (int i = 0; i<num_sensors; i++) {
+  for (int i = 0; i < num_sensors; i++) {
     int sensor_pin = SENSOR_PINS[i];
-    pinMode(sensor_pin, INPUT_PULLUP); 
+    pinMode(sensor_pin, INPUT_PULLUP);
   }
 }
 
 void setupSpeakers() {
-    pinMode(LED_BUILTIN, OUTPUT);
-    Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
 #if defined(__AVR_ATmega32U4__) || defined(SERIAL_USB) || defined(SERIAL_PORT_USBVIRTUAL)  || defined(ARDUINO_attiny3217)
-    delay(4000); // To be able to connect Serial monitor after reset or power up and before first print out. Do not wait for an attached Serial Monitor!
+  delay(4000); // To be able to connect Serial monitor after reset or power up and before first print out. Do not wait for an attached Serial Monitor!
 #endif
-    // Just to know which program is running on my Arduino
-    Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_TALKIE));
-    // voice.doNotUseInvertedOutput();
+  // Just to know which program is running on my Arduino
+  Serial.println(F("START " __FILE__ " from " __DATE__ "\r\nUsing library version " VERSION_TALKIE));
+  // voice.doNotUseInvertedOutput();
 #if defined(TEENSYDUINO)
-        pinMode(5, OUTPUT);
-        digitalWrite(5, HIGH); //Enable Amplified PROP shield
-    #endif
+  pinMode(5, OUTPUT);
+  digitalWrite(5, HIGH); //Enable Amplified PROP shield
+#endif
 }
 
 void loop() {
   updateReadings();
   updateLetter();
   speakCharacter();
-  delay(1000);  
+  delay(1000);
 }
 
 void updateReadings() {
-  for (int i = 0; i<num_sensors; i++) {
+  for (int i = 0; i < num_sensors; i++) {
     int sensor_pin = SENSOR_PINS[i];
     sensor_readings[i] = digitalRead(sensor_pin);
   }
 }
 
 void updateLetter() {
-  for (int i = 0; i<NUM_MAPPINGS; i++) {
+  for (int i = 0; i < NUM_MAPPINGS; i++) {
     boolean mapping_match = true;
-    for (int j = 0; j<num_sensors; j++) {
+    for (int j = 0; j < num_sensors; j++) {
       if (sensor_readings[j] != letter_sensor_mappings[i].sensor_readings[j]) {
         mapping_match = false;
         break;
@@ -95,7 +95,7 @@ void updateLetter() {
     }
     if (mapping_match) {
       interpreted_letter = letter_sensor_mappings[i].letter;
-      return;  
+      return;
     }
   }
 
@@ -103,68 +103,68 @@ void updateLetter() {
 }
 
 void speakCharacter() {
-      if (interpreted_letter == "a") {
-      voice.say(spa_A);
-    }
-    else if (interpreted_letter == "d") {
-      voice.say(spa_D);
-    }
-    else if (interpreted_letter == "f") {
-      voice.say(spa_F);
-    }
-    else if (interpreted_letter == "g") {
-      voice.say(spa_G);
-    }
-    else if (interpreted_letter == "h") {
-      voice.say(spa_H);
-    }
-    else if (interpreted_letter == "i") {
-      voice.say(spa_I);
-    }
-    else if (interpreted_letter == "k/p") {
-      voice.say(spa_K);
-      voice.say(spa_OR);
-      voice.say(spa_P);
-    }
-    else if (interpreted_letter == "l/q") {
-      voice.say(spa_L);
-      voice.say(spa_OR);
-      voice.say(spa_Q);
-    }
-    else if (interpreted_letter == "m") {
-      voice.say(spa_M);
-    }
-    else if (interpreted_letter == "n") {
-      voice.say(spa_N);
-    }
-    else if (interpreted_letter == "o") {
-      voice.say(spa_O);
-    }
-    else if (interpreted_letter == "r/u/v") {
-      voice.say(spa_R);
-      voice.say(spa_OR);
-      voice.say(spa_U);
-      voice.say(spa_OR);
-      voice.say(spa_V);
-    }
-    else if (interpreted_letter == "s") {
-      voice.say(spa_S);
-    }
-    else if (interpreted_letter == "t") {
-      voice.say(spa_T);
-    }
-    else if (interpreted_letter == "w") {
-      voice.say(spa_W);
-    }
-    else if (interpreted_letter == "x") {
-      voice.say(spa_X);
-    }
-    else if (interpreted_letter == "y") {
-      voice.say(spa_Y);
-    }
+  if (interpreted_letter == "a") {
+    voice.say(spa_A);
+  }
+  else if (interpreted_letter == "d") {
+    voice.say(spa_D);
+  }
+  else if (interpreted_letter == "f") {
+    voice.say(spa_F);
+  }
+  else if (interpreted_letter == "g") {
+    voice.say(spa_G);
+  }
+  else if (interpreted_letter == "h") {
+    voice.say(spa_H);
+  }
+  else if (interpreted_letter == "i") {
+    voice.say(spa_I);
+  }
+  else if (interpreted_letter == "k/p") {
+    voice.say(spa_K);
+    voice.say(spa_OR);
+    voice.say(spa_P);
+  }
+  else if (interpreted_letter == "l/q") {
+    voice.say(spa_L);
+    voice.say(spa_OR);
+    voice.say(spa_Q);
+  }
+  else if (interpreted_letter == "m") {
+    voice.say(spa_M);
+  }
+  else if (interpreted_letter == "n") {
+    voice.say(spa_N);
+  }
+  else if (interpreted_letter == "o") {
+    voice.say(spa_O);
+  }
+  else if (interpreted_letter == "r/u/v") {
+    voice.say(spa_R);
+    voice.say(spa_OR);
+    voice.say(spa_U);
+    voice.say(spa_OR);
+    voice.say(spa_V);
+  }
+  else if (interpreted_letter == "s") {
+    voice.say(spa_S);
+  }
+  else if (interpreted_letter == "t") {
+    voice.say(spa_T);
+  }
+  else if (interpreted_letter == "w") {
+    voice.say(spa_W);
+  }
+  else if (interpreted_letter == "x") {
+    voice.say(spa_X);
+  }
+  else if (interpreted_letter == "y") {
+    voice.say(spa_Y);
+  }
   Serial.print(interpreted_letter);
   Serial.print(": ");
-  for (int i = 0; i<num_sensors; i++) {
+  for (int i = 0; i < num_sensors; i++) {
     Serial.print(sensor_readings[i]);
   }
   Serial.println("");
